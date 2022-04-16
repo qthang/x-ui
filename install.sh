64ad14da8a7255db32cc809e55bc5fc536e9614a
qthang <quangthangdk95@gmail.com>
2022-04-17 00:27:19 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,23 +11,23 @@ cur_dir=$(pwd)
 # [[ $EUID -ne 0 ]] && echo -e "${red}错误：${plain} 必须使用root用户运行此脚本！\n" && exit 1
 
 # check os
-if [[ -f /etc/redhat-release ]]; then
-    release="centos"
-elif cat /etc/issue | grep -Eqi "debian"; then
-    release="debian"
-elif cat /etc/issue | grep -Eqi "ubuntu"; then
-    release="ubuntu"
-elif cat /etc/issue | grep -Eqi "centos|red hat|redhat"; then
-    release="centos"
-elif cat /proc/version | grep -Eqi "debian"; then
-    release="debian"
-elif cat /proc/version | grep -Eqi "ubuntu"; then
-    release="ubuntu"
-elif cat /proc/version | grep -Eqi "centos|red hat|redhat"; then
-    release="centos"
-else
-    echo -e "${red}未检测到系统版本，请联系脚本作者！${plain}\n" && exit 1
-fi
+# if [[ -f /etc/redhat-release ]]; then
+#     release="centos"
+# elif cat /etc/issue | grep -Eqi "debian"; then
+#     release="debian"
+# elif cat /etc/issue | grep -Eqi "ubuntu"; then
+#     release="ubuntu"
+# elif cat /etc/issue | grep -Eqi "centos|red hat|redhat"; then
+#     release="centos"
+# elif cat /proc/version | grep -Eqi "debian"; then
+#     release="debian"
+# elif cat /proc/version | grep -Eqi "ubuntu"; then
+#     release="ubuntu"
+# elif cat /proc/version | grep -Eqi "centos|red hat|redhat"; then
+#     release="centos"
+# else
+#     echo -e "${red}未检测到系统版本，请联系脚本作者！${plain}\n" && exit 1
+# fi
 
 arch=$(arch)
 
